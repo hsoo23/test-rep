ca44aeb33c336783f545ece91634f12bddfdca8c
2csmin <163493839+2csmin@users.noreply.github.com>
2024-03-22 19:33:58 +0900

    Update 0321학습.py

--- 0321학습.py
+++ 0321학습.py
@@ -4,4 +4,4 @@ Touched by user2
 print("print something")
 result_list = [''.join(w[::-1]) + w[-1].upper() if w[-1] not in 'aeiou' else w for word in word_list for w in word]
 print(result_list!!!)
-
+print("HI")
